f37f44043dd785012593934e9fc660a5ef1086b1
Mdubane Digits <65094648+Digital-101@users.noreply.github.com>
2024-10-25 13:28:20 +0200

    Updated

--- pybots/buysell.py
+++ pybots/buysell.py
@@ -1,38 +1,28 @@
-from datetime import datetime
 import backtrader as bt
+import yfinance as yf
 
 # Create a subclass of Strategy to define the indicators and logic
-
 class SmaCross(bt.Strategy):
-    # list of parameters which are configurable for the strategy
-    params = dict(
-        pfast=10,  # period for the fast moving average
-        pslow=30   # period for the slow moving average
-    )
+    params = dict(pfast=10, pslow=30)
 
     def __init__(self):
-        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
-        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
-        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+        sma1 = bt.ind.SMA(period=self.p.pfast)
+        sma2 = bt.ind.SMA(period=self.p.pslow)
+        self.crossover = bt.ind.CrossOver(sma1, sma2)
 
     def next(self):
-        if not self.position:  # not in the market
-            if self.crossover > 0:  # if fast crosses slow to the upside
-                self.buy()  # enter long
+        if not self.position:
+            if self.crossover > 0:
+                self.buy()
+        elif self.crossover < 0:
+            self.close()
 
-        elif self.crossover < 0:  # in the market & cross to the downside
-            self.close()  # close long position
+cerebro = bt.Cerebro()
 
+# Create a data feed with yfinance
+data = bt.feeds.PandasData(dataname=yf.download('MSFT', start='2024-07-01', end='2024-09-30'))
 
-cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
-
-# Create a data feed
-data = bt.feeds.YahooFinanceData(dataname='MSFT',
-                                 fromdate=datetime(2011, 1, 1),
-                                 todate=datetime(2012, 12, 31))
-
-cerebro.adddata(data)  # Add the data feed
-
-cerebro.addstrategy(SmaCross)  # Add the trading strategy
-cerebro.run()  # run it all
-cerebro.plot()  # and plot it with a single command
+cerebro.adddata(data)
+cerebro.addstrategy(SmaCross)
+cerebro.run()
+cerebro.plot()
--- pybots/getsignal.py
+++ pybots/getsignal.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 import backtrader as bt
+import yfinance as yf
 
-# Create a subclass of SignaStrategy to define the indicators and signals
-
+# Create a subclass of SignalStrategy to define the indicators and signals
 class SmaCross(bt.SignalStrategy):
-    # list of parameters which are configurable for the strategy
     params = dict(
         pfast=10,  # period for the fast moving average
         pslow=30   # period for the slow moving average
@@ -16,16 +15,14 @@ class SmaCross(bt.SignalStrategy):
         crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
         self.signal_add(bt.SIGNAL_LONG, crossover)  # use it as LONG signal
 
-
 cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
 
-# Create a data feed
-data = bt.feeds.YahooFinanceData(dataname='MSFT',
-                                 fromdate=datetime(2011, 1, 1),
-                                 todate=datetime(2012, 12, 31))
+# Create a data feed with yfinance
+data = bt.feeds.PandasData(
+    dataname=yf.download('MSFT', start='2024-08-01', end='2024-10-24')
+)
 
 cerebro.adddata(data)  # Add the data feed
-
 cerebro.addstrategy(SmaCross)  # Add the trading strategy
 cerebro.run()  # run it all
 cerebro.plot()  # and plot it with a single command
